40204af16c0c1ea49a845fe58b121d2abd7873e4
Peter Griess <pg@std.in>
2010-08-28 10:39:44 -0700

    Refactor app init / listen codepaths.

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require.paths.unshift("./vendor");
-require("./app");
-var router = require('./lib/router');
-var api = require('./lib/api');
 
-r = router.createRouter();
+require('./lib/router').createRouter().listen(80);
+require('./lib/api').createServer().listen(8081);
+require('./lib/app').boot(8080);
