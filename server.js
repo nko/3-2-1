26a55cdb44c57ffa3ce0f5597103530d524919d7
Peter Griess <pg@std.in>
2010-08-28 11:00:32 -0700

    Gather real request data from router.js.

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 require.paths.unshift("./vendor");
 
-require('./lib/router').createRouter().listen(80);
-require('./lib/api').createServer().listen(8081);
-require('./lib/app').boot(8080);
+var router = require('./lib/router').createRouter();
+var api = require('./lib/api').createServer();
+var app = require('./lib/app');
+
+api.addEventSource(router.ee);
+
+router.listen(80);
+api.listen(8081);
+app.boot(8080);
