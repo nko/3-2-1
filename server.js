24de834c87c51f624a6338c6f780211a5dfed3de
Tom Hughes-Croucher <tom_croucher@yahoo.com>
2010-08-28 14:51:19 -0700

    pointer server.js to use the right on method of router and bug fixed router.on

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var router = require('./lib/router').createRouter();
 var api = require('./lib/api').createServer();
 var app = require('./lib/app');
 
-api.addEventSource(router.ee);
+api.addEventSource(router);
 
 router.listen(80);
 api.listen(8081);
