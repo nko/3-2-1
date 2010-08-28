ee3689a2fb710f84ab6159840cbd970c9920194d
Reid Burke <me@reidburke.com>
2010-08-28 12:31:51 -0700

    Simple circle!

---
+++ public/app.js
@@ -0,0 +1,10 @@
+(function () {
+
+    var r = Raphael("realtime", 800, 400);
+
+    var circle = r.circle(50, 10, 10);
+
+    circle.attr("fill", "#d00");
+    circle.attr("stroke", "#000");
+
+})();
