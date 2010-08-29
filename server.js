7bfd6c80c88bf7d271cadfbcf3e287b93e1afcbd
Peter Griess <pg@std.in>
2010-08-28 17:31:13 -0700

    Basic CRUD /api/host API to touch routes.

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ require.paths.unshift("./vendor");
 var router = require('./lib/router').createRouter();
 var api = require('./lib/api').createServer();
 var app = require('./lib/app');
+var config = require('./lib/config').createConfig();
 
 api.setRouter(router);
+api.setConfig(config);
+// router.setConfig(config);
 
 router.listen(80);
 api.listen(8081);
