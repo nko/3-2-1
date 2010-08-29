3a5afd7d6aad24faf453d23845a7ce4cbf42c217
Tom Hughes-Croucher <tom_croucher@yahoo.com>
2010-08-28 18:43:20 -0700

    adding rules and fixing issues

--- server.js
+++ server.js
@@ -9,3 +9,36 @@ api.addEventSource(router);
 router.listen(80);
 api.listen(8081);
 app.boot(8080);
+
+//Hard block list
+router.on('clientReq', function(req, res) {
+    var blockedIps = []; 
+    for (var i=0; i<blockedIps.length;i++) {
+        if (req.connection.remoteAddress === blockedIps[i]) {
+            req.connection.end();
+            res.end();
+            router.stopPropegation('clientReq');
+            router.preventDefault('clientReq');
+            break;
+        }
+    }
+
+});
+
+//Soft block list
+router.on('clientReq', function(req, res) {
+    //var blockedIps = [];
+    var blockedIps = ['127.0.0.1'];
+    for (var i=0; i<blockedIps.length;i++) {
+        if (req.connection.remoteAddress === blockedIps[i]) {
+            var body = 'Denied'
+            res.writeHead(500, {'Content-Type': 'text/plain', 'Content-Length': body.length });
+            res.end(body);
+            req.connection.end();
+            router.stopPropegation('clientReq');
+            router.preventDefault('clientReq');
+            break;
+        }
+    }
+});
+
