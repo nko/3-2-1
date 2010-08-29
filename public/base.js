496dc7be28581b46302164cd46b200d5e64b8abc
Reid Burke <me@reidburke.com>
2010-08-29 15:41:53 -0700

    Real hostname!

--- public/base.js
+++ public/base.js
@@ -8,11 +8,22 @@ YUI.add("threel-base", function (Y) {
         Y.ThreeL.fire("socket:json", Y.JSON.parse(ev.data));
     };
 
+    Y.ThreeL.on("hostname", function (host) {
+        Y.one("#hostname").setContent(host);
+    });
+
+    Y.ThreeL.on("socket:json", function (d) {
+        d = d.req || d.res;
+        d = d.pop();
+        if (d.origin) Y.ThreeL.fire("hostname", d.origin);
+    });
+
     // transaction store
     var txns = {};
     Y.ThreeL.txns = txns;
 
 }, "0.0.1", { requires : [
     "json",
+    "node",
     "event-custom"
 ]});
